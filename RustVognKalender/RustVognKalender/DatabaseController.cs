3f532eb4dc9bf6b0116df1a5f25d71197fa782dd
Jes Lindekilde Johannsen <jesx0347@edu.eal.dk>
2018-12-06 10:20:37 +0100

    database controller skelet

--- RustVognKalender/RustVognKalender/DatabaseController.cs
+++ RustVognKalender/RustVognKalender/DatabaseController.cs
@@ -2,12 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace RustVognKalender
 {
     public class DatabaseController
     {
+        private string ConnectionString;
 
+        public DatabaseController()
+        {
+            StreamReader reader = new StreamReader(@"\..\..\ConnectionString.txt");
+            ConnectionString = reader.ReadLine();
+            reader.Close();
+        }
+
+        public bool CreateEvent(DateTime start, DateTime end, string Address, string Comment)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public bool AlterEvent(int PrimaryKey, string start = null, string end = null, string Address = null, string Comment = null)
+        {
+            Convert.ToDateTime(start);
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteEvent(string key)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
