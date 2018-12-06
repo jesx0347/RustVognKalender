99fa4d0888e138c5b14e4b9c28f8f34a61e0cfa0
Anders Zobbe Mortensen <ande097c@edu.eal.dk>
2018-12-06 11:21:03 +0100

    Controller

---
+++ RustVognKalender/RustVognKalender/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RustVognKalender
+{
+    class Controller
+    {
+        public bool CreateEventType(string start,string end, string address,string coment)
+        {
+            throw new NotImplementedException();
+        }
+        public bool AlterEvent(string key, string start, string end, string address, string coment)
+        {
+            throw new NotImplementedException();
+        }
+        public bool DeleteEvent(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
