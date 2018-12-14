f10383acad7b3914c886dc5eac9be775f990dfe4
Anders Zobbe Mortensen <ande097c@edu.eal.dk>
2018-12-14 12:54:28 +0100

    RustvognReposetory

--- RustVognKalender/EventLibary/Rustvogn.cs
+++ RustVognKalender/EventLibary/Rustvogn.cs
@@ -8,13 +8,37 @@ namespace EventLibary
 {
     public class Rustvogn
     {
+        public int Key;
         public int Prioritet;
         public status Status;
 
-        Rustvogn(int pri, status sta)
+        public Rustvogn(int pri, status sta)
         {
+            
             Prioritet = pri;
             Status = sta;
+        }
+        public Rustvogn(int key,int pri, status sta) : this(pri,sta)
+        {
+            Key = key;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is Rustvogn)
+            {
+                if (this.Prioritet == (obj as Rustvogn).Prioritet)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return base.Equals(obj);
+            }
         }
     }
 }
--- RustVognKalender/EventLibary/RustvognReposetory.cs
+++ RustVognKalender/EventLibary/RustvognReposetory.cs
@@ -12,13 +12,49 @@ namespace EventLibary
 
         public void AddRustvogn(Rustvogn rustvogn)
         {
-
             Rustvognen.Add(rustvogn);
         }
-        public void CreateRustvogn(int )
-        {
 
+
+        public void CreateRustvogn(int pri,status status)
+        {
+            foreach (Rustvogn i in Rustvognen)
+            {
+                if (pri == i.Prioritet)
+                {
+                    throw new MemberAccessException();
+                }
+            }
+            Rustvogn rustvogn = new Rustvogn(pri, status);
             Rustvognen.Add(rustvogn);
+            
+        }
+        public void alterRustvogn(int pri, int cpri)
+        {
+            foreach (Rustvogn i in Rustvognen)
+            {
+                if (pri == i.Prioritet)
+                {
+                    i.Prioritet = cpri;
+                }
+            }
+
+        }
+
+        public void getRustvogn(int pri)
+        {
+            Rustvogn rustvogn = new Rustvogn(pri, status.UnChanged);
+            Rustvognen.Add(rustvogn);
+        }
+        public void DeleteRustvogn(int pri)
+        {
+            foreach(Rustvogn i in Rustvognen)
+            {
+                if (pri == i.Prioritet)
+                {
+                    i.Status = status.Deleted;
+                }
+            }
         }
     }
 }
