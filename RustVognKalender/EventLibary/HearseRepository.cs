3e112f609c04588578ca3990cf2ef61dc0befd3a
Emil <emil428f@edu.eal.dk>
2018-12-18 08:35:32 +0100

    Converted class and methods from Danish to English

---
+++ RustVognKalender/EventLibary/HearseRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventLibary
+{
+    public class HearseRepository
+    {
+        List<Hearse> Hearse;
+
+        public void AddRustvogn(Hearse hearse)
+        {
+            Hearse.Add(hearse);
+        }
+
+
+        public void CreateHearse(int prio,status status)
+        {
+            foreach (Hearse i in Hearse)
+            {
+                if (prio == i.Priority)
+                {
+                    throw new MemberAccessException();
+                }
+            }
+            Hearse hearse = new Hearse(prio, status);
+            Hearse.Add(hearse);
+            
+        }
+        public void AlterHearse(int prio, int cpri)
+        {
+            foreach (Hearse i in Hearse)
+            {
+                if (prio == i.Priority)
+                {
+                    i.Priority = cpri;
+                }
+            }
+
+        }
+
+        public void GetHearse(int prio)
+        {
+            Hearse hearse = new Hearse(prio, status.UnChanged);
+            Hearse.Add(hearse);
+        }
+        public void DeleteHearse(int prio)
+        {
+            foreach(Hearse i in Hearse)
+            {
+                if (prio == i.Priority)
+                {
+                    i.Status = status.Deleted;
+                }
+            }
+        }
+    }
+}
